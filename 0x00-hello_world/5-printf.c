af497504f8233614b8310ba516fe463c49b126da
Minassie <minassieasfaw001@gmail.com>
2022-06-02 14:55:26 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/**
+ * main - hello, printf
+ *
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
