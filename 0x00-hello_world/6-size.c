3c30adf49f450d2aafb067b242a256d09db5a2e9
Minassie <minassieasfaw001@gmail.com>
2022-06-02 15:49:43 -0700

    Size matters

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,13 +5,14 @@
  */
 
 #include <stdio.h>
-int main (void)
+int main(void)
 {
 	char a;
 	int b;
 	long int  c;
 	long long int d;
 	float e;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(a));
 	printf("Size of an int: %ld byte(s)\n", sizeof(b));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
