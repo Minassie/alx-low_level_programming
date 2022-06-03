04ff00fb104fcf29809a471111230ad22a389b8b
Minassie <minassieasfaw001@gmail.com>
2022-06-03 15:59:23 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+/**
+ * main - print single digit base 10 whole numbers
+ *
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+	putchar (a);
+	}
+	putchar ('\n');
+	return (0);
+}
