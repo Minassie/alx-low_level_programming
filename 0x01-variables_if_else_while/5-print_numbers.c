f45b05563c91b79de9225a69bbae0fc1a4fe0283
Minassie <minassieasfaw001@gmail.com>
2022-06-03 15:42:25 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * main - A program that prints single digit base 10 whole numbers
+ *
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+
+	for(a = 0; a <= 9; a++)
+	{
+	printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
