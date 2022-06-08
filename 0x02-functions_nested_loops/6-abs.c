ce1286e1a417be99e48032f07105ff2f0953a5b2
Minassie <minassieasfaw001@gmail.com>
2022-06-07 17:27:37 -0700

    coompute the absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+/**
+ * _abs - computes the absolute value of an integer
+ * @j: The character is to be checked
+ *
+ * Return: 0
+ */
+
+#include "main.h"
+
+int _abs(int j)
+{
+	int k = -1;
+
+	if (j >= 0)
+	{
+		_putchar (j);
+	}
+	else
+	{
+		j = j * k;
+		_putchar (j);
+	}
+		return (0);
+}
