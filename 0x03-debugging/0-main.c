4c2077522f57b1fba121f6156200ae8ef9590574
Minassie <minassieasfaw001@gmail.com>
2022-06-09 02:14:08 -0700

    first exercise on debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,7 @@
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
+
 #include "main.h"
 
 int main(void)
@@ -9,7 +10,7 @@ int main(void)
 	int i;
 
 	i = 0;
-	postitive_or_negative(i);
+	positive_or_negative(i);
 
 	return (0);
 }
