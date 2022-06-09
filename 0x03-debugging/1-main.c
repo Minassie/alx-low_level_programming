5899a011373ad5b6586629dcbbdf891b1014c853
Minassie <minassieasfaw001@gmail.com>
2022-06-09 04:37:26 -0700

    Exausting

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -causes an infinite loop
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -21,4 +21,4 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
-}	
+}
