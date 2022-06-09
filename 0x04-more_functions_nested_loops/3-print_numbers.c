8d0382e9a7e0b6f7503e509d1a2839ac9992e3cf
Minassie <minassieasfaw001@gmail.com>
2022-06-09 16:57:29 -0700

    decimal digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ * print_numbers - prints all decimal digit
+ *
+ * Return: values
+ */
+
+#include "main.h"
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+		_putchar('\n');
+}
