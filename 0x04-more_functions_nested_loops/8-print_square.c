91ec301b08d83ba798a5bf02fbdf176960d2e992
Minassie <minassieasfaw001@gmail.com>
2022-06-09 20:35:57 -0700

    print squares

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,11 +15,15 @@ void print_square(int size)
 	}
 	else
 	{
-	for (a = 0; a <= size; a++)
+	for (a = 0; a < size; a++)
 	{
-		for (b = 0; b <= size; b++)
+		for (b = 0; b < size; b++)
 		{
 			_putchar('#');
+		}
+		if (a == size - 1)
+		{
+			continue;
 		}
 		_putchar('\n');
 	}
