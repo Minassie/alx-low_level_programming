80c2eeb399ece5415311327182f5d2146bf1c455
Minassie <minassieasfaw001@gmail.com>
2022-06-09 19:31:17 -0700

    diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonals
+ * @n: takes numbers
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	for (b = 0; b < n; b++)
+	{
+		for (a = 0; a < b; a++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
