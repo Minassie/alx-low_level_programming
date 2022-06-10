f9e5344ddf60b46389325d183134bd08285c17bf
Minassie <minassieasfaw001@gmail.com>
2022-06-09 20:09:57 -0700

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - prints square of # for given size
+ * @size - the variable that takes the size value
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 0; a <= size; a++)
+	{
+		for (b = 0; b <= size; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
