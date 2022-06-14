273699eecf632a5c857656c418157c0ea8e34721
Minassie <minassieasfaw001@gmail.com>
2022-06-13 17:53:29 -0700

    pointers_arrays&strings operations

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - a functions that prints strings
+ * @str: takes the string constants
+ */
+
+void _puts(char *str)
+{
+	do
+	{
+		_putchar(*str++);
+	}
+	while (*str != '\0');
+
+	_putchar('\n');
+}
