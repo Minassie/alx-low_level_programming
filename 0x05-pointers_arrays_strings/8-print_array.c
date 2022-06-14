4b697490019248af4f61e9888412ca501829f6b7
Minassie <minassieasfaw001@gmail.com>
2022-06-14 14:13:15 -0700

    printf

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+/**
+ * print_array - prints n elements in the given array
+ * @a: array to print
+ * @n: number of elements to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
