f6e5e752936a4c64b47f21a063fcf2cdb7a18705
Minassie <minassieasfaw001@gmail.com>
2022-06-20 23:11:33 -0700

    Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,13 +9,15 @@
  * Return: A pointer to dest.
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n)
+void *_memcpy(void *dest, const void *src, unsigned int n)
 {
 	unsigned int i;
+	unsigned char *destination = dest;
+	const unsigned char *source = src;
 
 	for (i = 0; i <= n; i++)
 	{
-		*(dest + i) = *(src + i);
+		*(destination + i) = *(source + i);
 	}
 	return (dest);
 }
