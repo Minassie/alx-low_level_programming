21f4d8d850f77a372746635df2fa3d490f1f1cde
Minassie <minassieasfaw001@gmail.com>
2022-07-07 07:30:26 -0700

    -re

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,3 +7,5 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif /* _VARIADIC_FUNCTIONS_H_ */
+
+
