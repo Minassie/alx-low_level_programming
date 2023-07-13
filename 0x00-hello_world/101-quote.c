e634bfb018c4799705dc98b519e22a6d053b3e64
Minassie <minassieasfaw001@gmail.com>
2023-07-13 15:25:34 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-# include <stdio.h>
-# include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - Hello printf
  * Return: 1 to the standard error
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59);
+	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
