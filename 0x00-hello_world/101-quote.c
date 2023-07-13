d54e4a377ddf2004ec4216fc5a3913791ca094ec
Minassie <minassieasfaw001@gmail.com>
2023-07-13 15:04:09 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+/**
+ * main - Hello printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora");
+	fprintf(stderr, " korpar, 2015-10-19\n");
+	return (0);
+}
